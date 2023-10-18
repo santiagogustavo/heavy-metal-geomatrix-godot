aec8d2b6552fb81a88666fec811a6143625b98de
santiagogustavo <santiago.gustavo.mail@gmail.com>
2023-10-18 12:59:12 -0300

    feat(pickups): implement R and B bases, move SFX to bases instead of pickup

--- Scripts/Characters/CharacterController.gd
+++ Scripts/Characters/CharacterController.gd
@@ -2,3 +2,4 @@ extends Node3D
 class_name CharacterController
 
 @export var body_slot: BoneAttachment3D
+@export var right_hand_slot: BoneAttachment3D
--- Scripts/Characters/InventoryManager.gd
+++ Scripts/Characters/InventoryManager.gd
@@ -6,8 +6,12 @@ class_name InventoryManager
 
 @onready var character_controller: CharacterController = $"../CharacterController"
 @onready var body_slot: BoneAttachment3D = character_controller.body_slot
+@onready var right_hand_slot: BoneAttachment3D = character_controller.right_hand_slot
 
 @export var body_instance: Node3D
+@export var right_hand_instance: Node3D
+
+@export var equip_type: Definitions.EquipType = Definitions.EquipType.Body
 
 func _process(_delta):
 	if body_instance:
@@ -15,9 +19,21 @@ func _process(_delta):
 
 func pick_up_item(slot: Definitions.EquipType, item: PackedScene):
 	if slot == Definitions.EquipType.Body:
-		if body_instance:
-			body_instance.queue_free()
-		body_instance = item.instantiate()
-		body_slot.add_child(body_instance)
-		if body_instance.name == 'Jetpack':
-			has_jetpack = true
+		clear_and_instantiate_body_item(item)
+	else:
+		equip_type = slot
+		clear_and_instantiate_right_hand_item(item)
+
+func clear_and_instantiate_body_item(item: PackedScene):
+	if body_instance:
+		body_instance.queue_free()
+	body_instance = item.instantiate()
+	body_slot.get_node("Offset").add_child(body_instance)
+	if body_instance.name == 'Jetpack':
+		has_jetpack = true
+
+func clear_and_instantiate_right_hand_item(item: PackedScene):
+	if right_hand_instance:
+		right_hand_instance.queue_free()
+	right_hand_instance = item.instantiate()
+	right_hand_instance.get_node("Offset").add_child(right_hand_instance)
