d909b9ba334ac33fb6be4af676efb83ce5c81bc8
santiagogustavo <santiago.gustavo.mail@gmail.com>
2024-09-09 00:48:29 -0300

    refactor(physics): update bullets collision detection and code

--- Scripts/Weapons/BulletController.gd
+++ Scripts/Weapons/BulletController.gd
@@ -1,48 +1,44 @@
 extends Node3D
 
-@export var speed = 50.0
+@export var speed: float = 50.0
 @export var hit_particle: PackedScene
 @export var hit_decal: PackedScene
 
-@onready var mesh = $Mesh
-@onready var raycast = $RayCast3D
+@onready var mesh: MeshInstance3D = $RigidBody3D/Mesh
+@onready var rigidBody = $RigidBody3D
 @onready var sfx = $SFX
 
-var has_collided = false
 
-func _process(delta):
-	if raycast.is_colliding() and !has_collided:
-		has_collided = true
-		collide()
-	elif !has_collided:
-		position += transform.basis * Vector3(0, 0, -speed) * delta
+func _physics_process(delta: float):
+	var collision: KinematicCollision3D = rigidBody.move_and_collide(transform.basis * Vector3(0, 0, -1) * speed * delta)
+	if collision:
+		collide(collision)
 
 func instantiate_decal(point: Vector3, normal: Vector3):
-	var decal_instance = hit_decal.instantiate()
+	var decal_instance: Node3D = hit_decal.instantiate()
 	get_tree().root.add_child(decal_instance)
-	
 	decal_instance.global_transform.origin = point
-	TransformUtils.safe_look_at(decal_instance, point + normal)
+	decal_instance.global_transform = TransformUtils.look_at_with_y(decal_instance.global_transform, normal, Vector3.UP)
 
 func instantiate_hit(point: Vector3, normal: Vector3):
-	var hit_instance = hit_particle.instantiate()
+	var hit_instance: Node3D = hit_particle.instantiate()
 	get_tree().root.add_child(hit_instance)
 	
 	hit_instance.global_transform.origin = point
-	TransformUtils.safe_look_at(hit_instance, point + normal)
+	hit_instance.global_transform = TransformUtils.look_at_with_y(hit_instance.global_transform, normal, Vector3.UP)
 	hit_instance.get_node("RaySpark").emitting = true
 
-func collide():
-	var collider = raycast.get_collider()
-	
+func collide(collision: KinematicCollision3D):
+	var collider: Object = collision.get_collider()
 	# if collider is world boundary
 	if collider.collision_layer == 16:
 		queue_free()
 	else:
-		var point = raycast.get_collision_point()
-		var normal = raycast.get_collision_normal()
+		var point: Vector3 = collision.get_position()
+		var normal: Vector3 = collision.get_normal()
+		print_debug(normal)
 		instantiate_hit(point, normal)
 		instantiate_decal(point, normal)
 		mesh.visible = false
 		sfx.play()
-		get_tree().create_timer(1).connect("timeout", queue_free)
+		get_tree().create_timer(0.1).connect("timeout", queue_free)
