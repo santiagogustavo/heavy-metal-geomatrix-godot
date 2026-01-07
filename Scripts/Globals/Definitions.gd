d2af244308c259dd9abe496f766b923cb26c9ccd
santiagogustavo <santiago.gustavo.mail@gmail.com>
2026-01-07 20:55:46 -0300

    refactor(kassey): remove obsolete assets, update v2 as default

--- Scripts/Globals/Definitions.gd
+++ Scripts/Globals/Definitions.gd
@@ -83,7 +83,7 @@ const Scenes: Dictionary = {
 const Selects: Dictionary = {
 	Characters.Mayfly: "res://Prefabs/Characters/Mayfly/Base.tscn",
 	Characters.Slash: "res://Prefabs/Characters/Slash/Base.tscn",
-	Characters.Kassey: "res://Prefabs/Characters/Kassey/BaseV2.tscn",
+	Characters.Kassey: "res://Prefabs/Characters/Kassey/Base.tscn",
 	Characters.Di: "res://Prefabs/Characters/Di/Base.tscn",
 	Characters.Hound: "res://Prefabs/Characters/Hound/Base.tscn"
 }
@@ -91,7 +91,7 @@ const Selects: Dictionary = {
 const Players: Dictionary = {
 	Characters.Mayfly: "res://Prefabs/Characters/Mayfly/Base.tscn",
 	Characters.Slash: "res://Prefabs/Characters/Slash/Base.tscn",
-	Characters.Kassey: "res://Prefabs/Characters/Kassey/BaseV2.tscn",
+	Characters.Kassey: "res://Prefabs/Characters/Kassey/Base.tscn",
 	Characters.Di: "res://Prefabs/Characters/Di/Base.tscn",
 	Characters.Hound: "res://Prefabs/Characters/Hound/Base.tscn",
 	Characters.Dummy: "res://Prefabs/Characters/Dummy/Player.tscn"
