a4f28d600b3c9d472e5ee3c1986f9d47b92842e3
santiagogustavo <santiago.gustavo.mail@gmail.com>
2026-05-26 22:52:51 -0300

    feat(weapons): introduce Spray Gun with paint splatter decal system

---
+++ Scripts/Decals/PaintSplatterDecal.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+@export var decals: Array[Decal]
+@export var color: Color
+@export_range(0.0, 10.0) var lifetime: float = 300.0
+
+var selected: Decal	
+var time_elapsed: float = 0.0
+
+func _ready() -> void:
+	selected = decals.pick_random()
+	selected.modulate = color
+	selected.visible = true
+
+func _process(delta: float) -> void:
+	if time_elapsed < lifetime:
+		time_elapsed += delta
+		var ratio = time_elapsed / lifetime
+		selected.modulate.a = lerpf(selected.modulate.a, 0.0, ratio)
